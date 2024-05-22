0f37c6ab9ae10738edfbb1fc2a8db6b915e760ca
cast1e <rooger233@gmail.com>
2024-05-22 09:05:09 +0800

    add README

---
+++ README.md
@@ -0,0 +1,13 @@
+# 无人机集群规划
+
+## 测试通过的编译平台
+
+Linux x86_64, Kernel: 6.8.9-arch1-2, gcc-14.1.1-posix
+
+## 编译
+
+ ```bash
+ cmake .
+ make 
+ ./bin/main
+ ```
--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main(){
     std::string filepath = "./output/";
     std::string ext = ".txt";
     std::ofstream file("./output/statistic.txt", std::ios::out);
-    for (double c = 0.5; c < 8; c+=0.5){
+    for (double c = 0.8; c < 1.0; c+=0.1){
         file << c << std::endl;
         for (int j = 0; j < 10; j++)
         {
             graph g(100, 200, 6, 5);
-            g.node_weight = c;
+            g.increase_rate = c;
             g.randomPoint(200);
             g.setDronePosition(10, 10);
             g.createMap();
